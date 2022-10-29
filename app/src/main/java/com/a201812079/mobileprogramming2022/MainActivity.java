53aa88059afd3ca4acf55f5778206b088298f00c
UIBIN-NOH <61104736+flex2020@users.noreply.github.com>
2022-10-29 19:37:11 +0900

    Inflation 예제 작성

--- app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
+++ app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
@@ -2,13 +2,50 @@ package com.a201812079.mobileprogramming2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout layout;
+    Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = (LinearLayout) findViewById(R.id.layout);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, layout, true);
+                button3 = (Button) findViewById(R.id.button3);
+                button3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // button3 이벤트작성
+                    }
+                });
+
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub2, layout, true);
+            }
+        });
+
+
     }
 }
