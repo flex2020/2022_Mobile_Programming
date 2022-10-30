e329994e58108cf164bd0ef2c2e88fb439f7b277
flex2020 <hosan_1@naver.com>
2022-10-30 11:47:35 +0900

    예제 4번 풀이

--- app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
+++ app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
@@ -2,13 +2,54 @@ package com.a201812079.mobileprogramming2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button, button2, button3;
+    LinearLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = (LinearLayout) findViewById(R.id.layout);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, layout, true);
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub2, layout, true);
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub3, layout, true);
+
+                Snackbar.make(view, "액티비티3", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
