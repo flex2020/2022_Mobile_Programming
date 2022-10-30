11c8dbd8b74a6ac7b05ac9da7858f8d6b17c44a9
flex2020 <hosan_1@naver.com>
2022-10-30 11:33:46 +0900

    예제 3번 풀이

--- app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
+++ app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
@@ -2,7 +2,11 @@ package com.a201812079.mobileprogramming2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View view = (View) findViewById(R.id.layout);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+
+                if(action == MotionEvent.ACTION_DOWN) {
+                    view.setBackgroundColor(Color.RED);
+                } else if(action == MotionEvent.ACTION_MOVE) {
+                    view.setBackgroundColor(Color.BLUE);
+                } else if (action == MotionEvent.ACTION_UP) {
+                    view.setBackgroundColor(Color.WHITE);
+                }
+
+
+                return true;
+            }
+        });
+
     }
+
+
 }
