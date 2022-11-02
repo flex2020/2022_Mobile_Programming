4f550ad65c00a9a12484438b6b0a740718f2dc9e
flex2020 <hosan_1@naver.com>
2022-11-02 16:49:05 +0900

    fragment 구현

--- app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
+++ app/src/main/java/com/a201812079/mobileprogramming2022/MainActivity.java
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    FirstFragment firstFragment;
+    SecondFragment secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+    }
+
+    public void onFragmentChange(int i) {
+        if(i == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+        }
     }
 }
